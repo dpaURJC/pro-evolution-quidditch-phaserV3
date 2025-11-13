c6834a1d8dc469a5e87c90700c99062975872428
dpaURJC <daniel.palacios@urjc.es>
2025-11-13 22:42:55 +0100

    Modificación de comentario

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const ProEvolutionQuidditch = { DEBUG: 0, VERSUS: 0 };
 
 // URL a la que quieres volver al final (puede ser './index.html', './', o una URL externa)
 const UI = {
-  HOMEPAGE_URL: './',      // Página inicial del sitio (ajústelo a su caso)
+  HOMEPAGE_URL: './',      // Página inicial del sitio (ajústala a tu caso)
   AUTO_HOME_AFTER_MS: 5000    // 0 = desactivado. Ej.: 15000 = volver a inicio a los 15 s
 };
 
